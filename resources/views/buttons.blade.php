063ff8d88673246ea8f33297042fbd0535413ab4
Vlad <salabunv@ukr.net>
2024-01-09 06:53:30 +0200

    buttons

--- resources/views/buttons.blade.php
+++ resources/views/buttons.blade.php
@@ -4,9 +4,45 @@
 @section('content')
 <div id="article" class="mt-3">
 <h1 style="text-align: center;">Кнопки</h1>
-<h2>...?</h2>
-<p></p>
 
+    <div class="p-3">
+        <h3>Види кнопок:</h3>
+        <div class="mb-3"> 
+ 
+            @foreach(["small", "middle", "big"] as $size)
+            <div class="d-flex flex-row">
+                @foreach(["green", "red", "default", "disabled"] as $type)
+                   <div class="m-1">
+                       
+                        @include('forms.button', [
+                            'id' => 'btn-' . $size . '-' . $type, // ідентифікатор кнопки
+                            'title' => 'Button ' . $size . ' ' . $type, // надпис на кнопці
+                           // 'title' => 'Ще одна кнопка', // надпис на кнопці
+                            'size' => $size, // small, middle, big
+                            'type' => $type, // green, red, disabled, default
+                        ])
+                   </div> 
+                @endforeach
+            </div>
+            @endforeach
+   
+        </div>
+
+
+
+        <h3>Код для копіювання:</h3>
+        <div class="mb-3"> 
+            @include("templates.code", [
+                "id" => "text",
+                'path' => "../resources/views/forms-usage/button-example.blade.php",
+                'lang' => "php",
+            ]) 
+        </div>
+        <div class="mb-3"> 
+            <p>Вказаний ідентифікатор слід передати обробнику Form Grabber.</p>
+        </div>
+
+    </div>
 </div>
 @endsection
 
---
+++ resources/views/forms-usage/button-example.blade.php
@@ -0,0 +1,6 @@
+@include('buttons.button-template', [
+    'id' => '', // ідентифікатор кнопки
+    'title' => 'Button', // надпис на кнопці
+    'size' => 'small', // small, middle, big
+    'type' => 'green', // green, red, disabled, default
+])
